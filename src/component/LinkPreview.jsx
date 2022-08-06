09a88255cb1b0748120a9939730c97c1f899ef7b
avinashinstep <106313733+avinashinstep@users.noreply.github.com>
2022-08-06 12:20:11 +0530

    changes link tag

--- src/component/LinkPreview.jsx
+++ src/component/LinkPreview.jsx
@@ -7,11 +7,10 @@ import Typography from '@mui/material/Typography';
 
 export function LinkPreview({ url }) {
     const { metadata, isLoading, isError } = useLinkPreview(url)
-    console.log('>>>', metadata)
     const style = {
         mainCard: {
             width: '450px',
-            height: '400px',
+            height: '420px',
             marginLeft: '50%',
             transform: 'translateX(-50%)',
             marginTop: '40px'
@@ -21,7 +20,6 @@ export function LinkPreview({ url }) {
         }
 
     }
-
     return (
         <>
             {isLoading && <h2> Loading... </h2>}
@@ -45,8 +43,9 @@ export function LinkPreview({ url }) {
                                         {metadata.description}
                                     </Typography>
                                     <Typography variant="body2" color="text.secondary" >
-                                        URL:{metadata.requestUrl}
+                                        <a href={metadata.requestUrl} target='_blank' >URL:{metadata.requestUrl}</a>
                                     </Typography>
+
                                 </CardContent>
 
                             </Card>
